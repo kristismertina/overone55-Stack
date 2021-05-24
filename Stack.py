6b875c495c03c9d10828ab14ec89e52f49e8b6ec
kris <kristismertina@gmail.com>
2021-05-24 11:45:54 +0300

    insert_delite_view

--- Stack.py
+++ Stack.py
@@ -3,4 +3,11 @@ class Stack:
     def __init__ (self):
         self.date = []
 
-   
+    def insert (self, value):
+        self.date.append (value)
+
+    def pop (self):
+       return self.date.pop ()
+
+    def view (self):
+        return self.date [len(self.date)-1]
